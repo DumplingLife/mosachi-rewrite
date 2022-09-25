3b8f729c39f11a6e98cfb4376b806cc284a27ae2
Jason Cheng <60458933+DumplingLife@users.noreply.github.com>
2022-09-25 10:20:50 -0700

    Revert "Jason"

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const synergy = require('./synergy.js');
-const textNotifications = require('./text-notifications/textNotifications.js');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/mosachi_rewrite_db');
 
 const PORT = 3000;
 
@@ -40,32 +37,6 @@ app.post('/logout', (req, res) => {
 app.get('/get-gradebook', (req, res) => {
     res.json(req.session.gradebook);
 });
-
-app.post('/text-notifications/create', (req, res) => {
-    console.log(req.body);
-    synergy.getGradebook(req.body.username, req.body.password, req.body.urlSubdomain).then((gradebook) => {
-        if(gradebook == synergy.INVALID_CREDENTIALS_STR) res.send(false);
-        else {
-            mmsEmail = req.body.phoneNumber + "@" + req.body.mmsEmailGateway;
-            textNotifications.createUser(req.body.username, req.body.password, req.body.urlSubdomain, mmsEmail, gradebook);
-            textNotifications.sendText(mmsEmail, 'Welcome to Mosachi text notifications! To stop receiving texts, go to mosachi.org and delete your account.');
-            res.send(true);
-        }
-    });
-});
-app.post('/text-notifications/delete', (req, res) => {
-    console.log(req.body);
-    synergy.getGradebook(req.body.username, req.body.password, req.body.urlSubdomain).then((gradebook) => {
-        if(gradebook == synergy.INVALID_CREDENTIALS_STR) res.send(false);
-        else {
-            textNotifications.deleteUser(req.body.username, req.body.urlSubdomain).then((deletedCount) => {
-                if(deletedCount >= 1) res.send(true);
-                else res.send(false);
-            });
-        }
-    });
-});
-
 
 //frontend routes
 app.get('/login', (req, res) => {
